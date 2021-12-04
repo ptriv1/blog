4f1cac516a30e71e5737c910441da3788bb5f366
Galatia <ptrivedius@aol.com>
2021-12-04 13:07:59 -0500

    changes to connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,13 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
+const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306 
+});
+
+/*
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -18,5 +25,7 @@ if (process.env.JAWSDB_URL) {
         }
     );
 }
+
+*/
 
 module.exports = sequelize;
