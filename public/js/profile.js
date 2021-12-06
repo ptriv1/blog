adc70fd07dfe87322f0b50cef855f16144d063de
Galatia <ptrivedius@aol.com>
2021-12-06 11:47:41 -0500

    pushing current code

--- public/js/profile.js
+++ public/js/profile.js
@@ -7,7 +7,7 @@ const newFormHandler = async (event) => {
     if (name && description) {
         const response = await fetch(`/api/posts`, {
             method: 'POST',
-            body: JSON.stringify({ name, description }),
+            body: JSON.stringify({ name, comment });
             headers: {
                 'Content-Type': 'application/json',
             },
